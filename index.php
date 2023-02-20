926a6d507a24592e8a3d570d21ee3090d177d687
MylesDenommeDev <125718709+MylesDenommeDev@users.noreply.github.com>
2023-02-20 00:00:13 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie-edge">
+
+	
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<!--Import two Google fonts: Poppins, Manrope -->
+	<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@800&family=Open+Sans:wght@300;500&family=Poppins:ital,wght@0,400;1,600&display=swap" rel="stylesheet">
+	
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+		  integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+		  crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"
+			integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ=="
+			crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js"
+			integrity="sha512-57oZ/vW8ANMjR/KQ6Be9v/+/h6bq9/l3f0Oc7vn6qMqyhvPd1cvKBRWWpzu0QoneImqr2SkmO4MSqU+RpHom3Q=="
+			crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+	<link rel="stylesheet" type="text/css" href="css/main-style.css">
+	<title>Rebel Admin Panel</title>
+</head>
+
+<body>
+	<div id="master-container">
+		<div id="header">
+			<span name="rebel-icon" style="position: absolute; top: 0.5vh;">
+			<svg aria-labelledby="navbar-rebel-com-logo" height="5vh" role="img" viewBox="0 0 572 202" class="sc-gKXOVf eDzBng"><title id="navbar-rebel-com-logo">rebel-logo-2021</title><path d="M146.2,123.2c0-32.9,24.1-57,58.3-57s57.6,24.1,57.6,57v9.2h-81.7c2.6,12.7,12.3,18.4,24.1,18.4
+		c9.4,0,16.4-2,20.6-7.9h34c-7,22.3-27.6,37.2-54.5,37.2C170.3,180.2,146.2,156.1,146.2,123.2z M228.4,111.6
+		c-3.7-11.2-12.5-16-23.9-16c-11.4,0-20.2,5-23.4,16H228.4z" fill="rgb(69, 62, 97)"></path><path d="M388,123.2c0-32.9,24.1-57,58.3-57s57.6,24.1,57.6,57v9.2h-81.7c2.6,12.7,12.3,18.4,24.1,18.4
+		c9.4,0,16.4-2,20.6-7.9h34c-7,22.3-27.6,37.2-54.5,37.2C412.1,180.2,388,156.1,388,123.2z M470.1,111.6c-3.7-11.2-12.5-16-23.9-16
+		c-11.4,0-20.2,5-23.4,16H470.1z" fill="rgb(69, 62, 97)"></path><path d="M330.8,66.3c-10.4,0-21.4,3.2-30.7,9.8V21.8h-32.9v100.3c0,36.4,24.5,58.1,57.6,58.1
+		c34.4,0,58.5-23.4,58.5-58.3C383.3,84.2,355.3,66.3,330.8,66.3z M325.1,149.5c-14.9,0-25-10.7-25-26.3c0-15.6,10.1-26.3,25-26.3
+		c14.9,0,25,10.7,25,26.3C350,138.8,340,149.5,325.1,149.5z" fill="rgb(69, 62, 97)"></path><path d="M29.1,22.2h62.6c40.9,0,59.1,22.2,59.1,52.9c0,21.7-9.2,39.5-29.1,47.7l34.7,53.9h-45.4l-27.4-48.5H67.4v48.5
+		H29.1V22.2z M92.3,92.9c15.4,0,19.9-7.6,19.9-17.7s-4.5-17.5-19.9-17.5H67.4v35.2H92.3z" fill="rgb(69, 62, 97)"></path><rect x="510" y="21.8" width="32.9" height="155" fill="rgb(69, 62, 97)"></rect></svg>
+
+			<span class="admin-panel-text">- admin panel -</span>
+			</span>
+
+			<div class="header-box-right">
+				<div class="header-button" data-navButton="accounting">Accounting</div>
+				<div class="header-button" data-navButton="analytics">Analytics</div>
+			</div>
+		</div><!--_End of Header_-->
+		
+		<div id="main-container">
+
+		</div>
+
+
+		<!--<h1>Accounting</h1>-->
+	</div>
+
+	<!--This is a special div, placed at the end, that will be positioned absolutely and should not impact any elements above-->
+	<div id="man001-box">
+	</div>
+
+</body>
+
+<!---___Scripts___--->
+<script language="JavaScript" type="text/javascript" src="js/client-functionality.js" defer>
+</script>
+
+</html>
